961a174f141734946e4f6809e053d53e3b9b4fff
mgiurato <mattia.giurato@polimi.it>
2020-06-03 22:14:48 +0200

    implemented timeout feature for wifi settings

--- code/sensor_module/sensor_module.ino
+++ code/sensor_module/sensor_module.ino
@@ -10,21 +10,24 @@
 #include <Adafruit_BME280.h>
 
 #define FACTORY_RESET 0
-#define MODULE_TYPE "Sensor module"
+#define MODULE_TYPE "SM_%08X"
 
 #define CHANNEL_ID_SIZE 7+1
 #define WRITE_API_KEY_SIZE 16+1
 
 #define BME_POWER_PIN D4
+#define BME_GND_PIN D3
 #define BME_I2C_ADDRESS 0x76
 
 #define VOLTAGE_PIN A0
-#define VOLTAGE_GAIN 4.5/1023.0
+#define VOLTAGE_GAIN 4.2/1023.0
 #define VOLTAGE_THRESHOLD 3.5
+#define VOLTAGE_MEASURE_ITERATIONS 5
 
 #define SERIAL_BAUD 115200
-#define REGULAR_TIMEOUT 60e6
-#define LOW_BATTERY_TIMEOUT 5*60e6
+#define TIMEOUT_CONFIG_PORTAL 300 //seconds
+#define REGULAR_TIMEOUT 300 //seconds
+#define LOW_BATTERY_TIMEOUT 600 //seconds
 
 char channel_id[CHANNEL_ID_SIZE];
 char write_api_key[WRITE_API_KEY_SIZE];
@@ -99,6 +102,9 @@ void setup() {
   //set config save notify callback
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
+  //set timeout for wifi connection trials
+  wifiManager.setConfigPortalTimeout(TIMEOUT_CONFIG_PORTAL);
+
   //add all your parameters here
   wifiManager.addParameter(&custom_channel_id);
   wifiManager.addParameter(&custom_write_api_key);
@@ -108,89 +114,97 @@ void setup() {
     wifiManager.resetSettings();
   }
 
-  wifiManager.autoConnect(MODULE_TYPE);
+  char APssid[25];
+  snprintf(APssid, 25, MODULE_TYPE, ESP.getChipId());
 
-  //read updated parameters
-  strcpy(channel_id, custom_channel_id.getValue());
-  strcpy(write_api_key, custom_write_api_key.getValue());
+  if (!wifiManager.autoConnect(APssid)) {
+    Serial.println("Failed to connect and hit timeout");
+    ESP.deepSleep(REGULAR_TIMEOUT * 1000000);
+  } else {
+    //read updated parameters
+    strcpy(channel_id, custom_channel_id.getValue());
+    strcpy(write_api_key, custom_write_api_key.getValue());
 
-  //save the custom parameters to FS
-  if (shouldSaveConfig) {
-    Serial.println("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["channel_id"] = channel_id;
-    json["write_api_key"] = write_api_key;
+    //save the custom parameters to FS
+    if (shouldSaveConfig) {
+      Serial.println("saving config");
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& json = jsonBuffer.createObject();
+      json["channel_id"] = channel_id;
+      json["write_api_key"] = write_api_key;
 
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
+      File configFile = SPIFFS.open("/config.json", "w");
+      if (!configFile) {
+        Serial.println("failed to open config file for writing");
+      }
+
+      json.printTo(Serial);
+      json.printTo(configFile);
+      configFile.close();
+    }
+    //end save
+
+    Serial.println();
+    Serial.println("local ip");
+    Serial.println(WiFi.localIP());
+
+    ThingSpeak.begin(client);  // Initialize ThingSpeak
+
+    pinMode(BME_POWER_PIN, OUTPUT);
+    digitalWrite(BME_POWER_PIN, HIGH);
+    pinMode(BME_GND_PIN, OUTPUT);
+    digitalWrite(BME_GND_PIN, LOW);
+    delay(500);
+
+    bme.begin(BME_I2C_ADDRESS);
+
+    float t = bme.readTemperature();
+    float h = bme.readHumidity();
+    float p = bme.readPressure() / 100.0F;
+
+    digitalWrite(BME_POWER_PIN, LOW);
+    delay(500);
+
+    float v = 0.0;
+    for (int i = 0; i < VOLTAGE_MEASURE_ITERATIONS; i++) {
+      v = (float)analogRead(VOLTAGE_PIN) * VOLTAGE_GAIN + v;
+      delay(100);
+    }
+    v = v / VOLTAGE_MEASURE_ITERATIONS;
+
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" degrees Celsius, Humidity: ");
+    Serial.print(h);
+    Serial.print(" %, Pressure: ");
+    Serial.print(p);
+    Serial.print(" hPa, Battery voltage: ");
+    Serial.print(v);
+    Serial.println(" V. Send to Thingspeak.");
+
+    ThingSpeak.setField(temperature_field, t);
+    ThingSpeak.setField(humidity_field, h);
+    ThingSpeak.setField(pressure_field, p);
+    ThingSpeak.setField(battery_voltage_field, v);
+
+    if ( v <= VOLTAGE_THRESHOLD )  {
+      ThingSpeak.setStatus("Low battery!");
+    } else {
+      ThingSpeak.setStatus("Operative and running.");
     }
 
-    json.printTo(Serial);
-    json.printTo(configFile);
-    configFile.close();
-  }
-  //end save
+    statusCode = ThingSpeak.writeFields(atoi(channel_id), write_api_key);
+    if (statusCode == 200) {
+      Serial.println("Channel update successful.");
+    } else {
+      Serial.println("Problem updating channel. HTTP error code " + String(statusCode));
+    }
 
-  Serial.println();
-  Serial.println("local ip");
-  Serial.println(WiFi.localIP());
-
-  ThingSpeak.begin(client);  // Initialize ThingSpeak
-
-  pinMode(BME_POWER_PIN, OUTPUT);
-  digitalWrite(BME_POWER_PIN, HIGH);
-  delay(500);
-
-  bme.begin(BME_I2C_ADDRESS);
-
-  float t = bme.readTemperature();
-  float h = bme.readHumidity();
-  float p = bme.readPressure() / 100.0F;
-
-  digitalWrite(BME_POWER_PIN, LOW);
-  delay(500);
-
-  float v = 0;
-  for (int i = 0; i < 10; i++) {
-    v = (float)analogRead(VOLTAGE_PIN) * VOLTAGE_GAIN + v;
-    delay(10);
-  }
-  v = v / 10;
-
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" degrees Celsius, Humidity: ");
-  Serial.print(h);
-  Serial.print(" %, Pressure: ");
-  Serial.print(p);
-  Serial.print(" hPa, Battery voltage: ");
-  Serial.print(v);
-  Serial.println(" V. Send to Thingspeak.");
-
-  ThingSpeak.setField(temperature_field, t);
-  ThingSpeak.setField(humidity_field, h);
-  ThingSpeak.setField(pressure_field, p);
-  ThingSpeak.setField(battery_voltage_field, v);
-
-  if ( v <= VOLTAGE_THRESHOLD )  {
-    ThingSpeak.setStatus("Low battery!");
-  } else {
-    ThingSpeak.setStatus("Operative and running.");
-  }
-
-  statusCode = ThingSpeak.writeFields(atoi(channel_id), write_api_key);
-  if (statusCode == 200) {
-    Serial.println("Channel update successful.");
-  } else {
-    Serial.println("Problem updating channel. HTTP error code " + String(statusCode));
-  }
-
-  if ( v <= VOLTAGE_THRESHOLD )  {
-    ESP.deepSleep(LOW_BATTERY_TIMEOUT);
-  } else {
-    ESP.deepSleep(REGULAR_TIMEOUT);
+    if ( v <= VOLTAGE_THRESHOLD )  {
+      ESP.deepSleep(LOW_BATTERY_TIMEOUT * 1000000);
+    } else {
+      ESP.deepSleep(REGULAR_TIMEOUT * 1000000);
+    }
   }
 }
 
